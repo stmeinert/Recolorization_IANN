bca41e045085fabf26e900a194170e4e29d6da93
jalippold <jlippold@uos.de>
2022-03-07 15:44:14 +0100

    Added sample image output

--- main.py
+++ main.py
@@ -37,6 +37,11 @@ if __name__ == '__main__':
     # check how model performs on train data once before we begin
     train_loss, _ = test(model, train_ds, loss)
     train_losses.append(train_loss)
+
+    # Sets up a timestamped log directory.
+    logdir = "logs/train_data/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+    # Creates a file writer for the log directory.
+    file_writer = tf.summary.create_file_writer(logdir)
     
     #   We train for num_epochs epochs.
     for epoch in range(NUM_EPOCHS):
@@ -56,14 +61,25 @@ if __name__ == '__main__':
         diff_time = datetime.datetime.now() - start_time
         print(f"Epoch {epoch} took {diff_time} to complete.")
 
+        images = []
+        for input, target in test_ds.take(25):
+            prediction = model(input)
 
-    # Sets up a timestamped log directory.
-    #logdir = "logs/train_data/" + datetime.now().strftime("%Y%m%d-%H%M%S")
-    # Creates a file writer for the log directory.
-    #file_writer = tf.summary.create_file_writer(logdir)
+            # get l channel, target should be in shape (SIZE, SIZE, lab)
+            l = tf.slice(target, begin=[0,0,0], size=[-1,-1,1])
+            prediction = tf.concat([l, prediction], axis=-1) # should be concatenating along last dimension
 
-    #with file_writer.as_default():
-    #    tf.summary.image("First dimension", train_ds[:25], max_outputs=25, step=0)
+            # convert prediction and target back to rgb, input to [0;1]
+            input = (input+1)/2
+            prediction = tfio.experimental.color.lab_to_rgb(prediction)
+            target = tfio.experimental.color.lab_to_rgb(target)
+
+            images.append((input, prediction, target))
+
+            with file_writer.as_default():
+                tf.summary.image(f'Epoch{epoch}', images, max_outputs=25, step=0)
+
+
 
     # Load the TensorBoard notebook extension.
     #%load_ext tensorboard
